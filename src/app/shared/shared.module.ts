995c1bfb65bda0d718adcadb411939a07c3742dc
Your NameRafael <rafaaquino28@gmail.com>
2026-04-06 09:34:34 -0300

    feat: shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatIconModule } from "@angular/material/icon";
+
+import { HeaderComponent } from "./layouts/header/header.component";
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+  ],
+  exports: [HeaderComponent],
+})
+export class SharedModule {}
